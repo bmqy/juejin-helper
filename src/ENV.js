5f8ea32d713e6eb6602164587c10af951aae0baa
北门清燕 <qq88268459@126.com>
2025-02-26 09:41:51 +0800

    Update ENV.js

--- src/ENV.js
+++ src/ENV.js
@@ -1,6 +1,7 @@
 module.exports = {
   COOKIE: process.env.COOKIE,
   EMAIL: process.env.EMAIL,
+  SMTP_HOST: process.env.SMTP_HOST,
   AUTHORIZATION_CODE: process.env.AUTHORIZATION_CODE,
   PUSHPLUS_TOKEN: process.env.PUSHPLUS_TOKEN,
   DINGDING_WEBHOOK: process.env.DINGDING_WEBHOOK,
